e9d4b5c904f15b94372ae420a6a897ee090d13ac
AminASTU <71122113+AminASTU@users.noreply.github.com>
2021-12-28 09:06:12 +0400

    done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var User = require("../Messenger/user.js");
 var MsgOnClient = require("../Messenger/message.js");
+var Private = require("../Messenger/private.js");
+var uuid = require("../Messenger/node_modules/uuid");
 
 var express = require("express");   // подключение модуля express
 const { stringify } = require("querystring");
@@ -10,6 +12,7 @@ var server = require('http').createServer(app); // подключаем http, с
 var io = require('socket.io')(server);   // подключаем socket.io. Отслеживаем наш сервер
 
 var User = require('./user');   // подключение user
+const { setPriority } = require("os");
 
 
 server.listen(3000); // отслеживаем порт 3000
@@ -18,19 +21,38 @@ server.listen(3000); // отслеживаем порт 3000
 });*/
 /* выводим главную страницу при запуске */
 app.get('/', function(request, response){
-    response.sendFile(__dirname + "/pages/chat.html");   // отправляем файл index.html при запуске
-});
-app.get('/pages/group.html', function(request, response){
-    response.sendFile(__dirname + "/pages/group.html"); 
-});
-app.get('/pages/personal.html', function(request, response){
-    response.sendFile(__dirname + "/pages/personal.html"); 
+    response.sendFile(__dirname + "/pages/chat.html");   // отправляем файл chat.html при запуске
 });
 
 var users = new Map();
-
+var story = new Map();
+privates = []; // лс
 online = [];   // онлайн челы
 let count = 0;
+
+// Обновление ника в общем списке
+const updateNick = (username) => {
+    
+};
+
+// Если такая приватная комната уже создана
+const findPrivateRoom = (id) => {
+    for (let item of privates) {
+        if (item.id == id)
+            return item;
+    }
+    return null;
+};
+
+// Поиск приватной комнаты по двум собеседникам
+const findPrivateRoomById = (first, second) => {
+    for (let item of privates) {
+        if ((item.firstID == first && item.secondID == second) || (item.secondID == first && item.firstID == second))
+            return item;
+    }
+    return null;
+}
+story.set("general", []);
 
  /* при подключении (событие) */
 io.sockets.on('connection', async (socket) => {
@@ -44,12 +66,33 @@ io.sockets.on('connection', async (socket) => {
     io.emit('list-users', online);
 
     socket.on('join-room', function(room){    // событие входа в комнату
+        
+        idRoom = findPrivateRoom(room);
+        // Если подкл не в приватную комнату
+        if (idRoom != null && idRoom.firstID != socket.id && idRoom.secondID != socket.id) {return;}    
+
+        socket.removeAllListeners('msg-room');
         socket.join(room);
+        socket.emit('new-chat');
+
+        if(!story.get(room)) {  // Если клиент заходит в уже сущ. комнату ему показывается история смс
+            story.set(room, []);
+        }
+        else{
+            if(story.get(room) != null){
+                for (const item of story.get(room)) {
+                    socket.emit('add-msg', item);
+                }
+            }
+        }
 
         socket.on('msg-room', function(Msg){    // смс в комнату
             var user = users.get(socket.id);
             var onClient = new MsgOnClient(Msg.time, Msg.content, user);
             console.log("Комната: " + room + " Время: ", onClient.time, " смс: ", onClient.content, " пользователь: ", onClient.User.name);
+            if(story.get(room) != null){
+                story.get(room).push(onClient);
+            }
             io.to(room).emit('add-msg', onClient);
         });
 
@@ -61,13 +104,34 @@ io.sockets.on('connection', async (socket) => {
                 user.name = username;
             }  
         });
-
         socket.on('leave-room', () =>{
             socket.leave(room);
-            socket.removeAllListeners('msg-room');
         });
     });
-    
+
+    socket.on('update-users',()=>{
+        online = [];
+        for (let item of users.keys()) {
+            online.push(new User(item, users.get(item).name));
+        }
+        io.emit('list-users', online);
+    });
+
+    socket.on('private-room', (firstId) =>{
+        let idRoom;
+        let room = findPrivateRoomById(firstId, socket.id);
+        if (room == null) {
+            idRoom = uuid.v4();
+            while (story.has(idRoom) || findPrivateRoom(idRoom) != null) {
+                idRoom = uuid.v4();
+            }
+            privates.push(new Private(idRoom, firstId, socket.id));
+        }
+        else {
+            idRoom = room.id;
+        }
+        io.sockets.emit('create-room', idRoom);
+    });
     socket.on('get-users', () =>{
         names = [];
         id = []
